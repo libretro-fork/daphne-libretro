8566ee2b3274e1406397cca05df66a8f3276b4f8
twinaphex <libretro@gmail.com>
2018-05-12 02:58:40 +0200

    Slim down libvo

--- src/vldp2/libvo/video_out.c
+++ src/vldp2/libvo/video_out.c
@@ -34,10 +34,6 @@
 
 /* Externally visible list of all vo drivers */
 
-extern vo_open_t vo_xv_open;
-extern vo_open_t vo_x11_open;
-extern vo_open_t vo_dxrgb_open;
-extern vo_open_t vo_dx_open;
 extern vo_open_t vo_sdl_open;
 extern vo_open_t vo_null_open;
 extern vo_open_t vo_nullslice_open;
